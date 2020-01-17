03d0d030a6f094c447d32e8c34d45bfe14c5e7b4
Soksijada <k.bakovic7@gmail.com>
2020-01-17 12:02:06 +0100

    AddShift UI is finished.

--- Unpause/Unpause/Views/AddShiftViewController.swift
+++ Unpause/Unpause/Views/AddShiftViewController.swift
@@ -22,23 +22,22 @@ class AddShiftViewController: UIViewController {
     private let arriveImageView = UIImageView()
     
     private let arrivalDateLabel = UILabel()
-    private let arrivalTimePicker = UIPickerView()
+    private let arrivalTimeLabel = UILabel()
     private let renameArrivalTimeButton = UIButton()
     
-    private let youAreLeavingAt = UILabel()
+    private let youAreLeavingAtLabel = UILabel()
     private let leavingImageView = UIImageView()
     
-    private let leavingDatePicker = UIPickerView()
-    private let renameLeavingDateButton = UIButton()
-    private let leavingTimePicker = UIPickerView()
-    private let renameLeavingTimeButton = UIButton()
+    private let leavingDateLabel = UILabel()
+    private let leavingTimeLabel = UILabel()
     
     private let separator = UIView()
-    
     private let descriptionLabel = UILabel()
     
-    private let cancleButton = UIButton()
-    private let continueButton = UIButton()
+    private let stackView = UIStackView()
+    
+    private let cancleButton = OrangeButton(title: "Cancle")
+    private let continueButton = OrangeButton(title: "Continue")
     
     init(addShiftViewModel: AddShiftViewModel) {
         self.addShiftViewModel = addShiftViewModel
@@ -58,8 +57,12 @@ class AddShiftViewController: UIViewController {
         configureScrollViewAndContainerView()
         renderAddingShiftLabelAndAddingShiftSeparator()
         renderArrivedAtLabelAndArriveImageView()
+        renderArrivalPickersAndLabelsForDateAndTime()
+        renderLeavingAtLabelAndLeavingImageView()
+        renderLeavingPickersAndLabelsForDateAndTime()
+        renderSeparatorAndDescription()
+        renderCancleAndContinueButton()
     }
-    
 }
 
 private extension AddShiftViewController {
@@ -109,6 +112,105 @@ private extension AddShiftViewController {
         }
         youArrivedAtLabel.text = "You arrived at:"
         youArrivedAtLabel.font = youArrivedAtLabel.font.withSize(20)
+        
+        containerView.addSubview(arriveImageView)
+        arriveImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(youArrivedAtLabel.snp.bottom).offset(15)
+            make.left.equalToSuperview().offset(18)
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+        }
+        arriveImageView.image = UIImage(named: "enter_500x500")
+        arriveImageView.contentMode = .scaleAspectFit
+    }
+    
+    func renderArrivalPickersAndLabelsForDateAndTime() {
+        containerView.addSubview(arrivalDateLabel)
+        arrivalDateLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(youArrivedAtLabel.snp.bottom).offset(40)
+            make.left.equalTo(arriveImageView.snp.right).offset(30)
+        }
+        arrivalDateLabel.text = "13.01.2020"
+        
+        containerView.addSubview(arrivalTimeLabel)
+        arrivalTimeLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(arrivalDateLabel.snp.bottom).offset(15)
+            make.left.equalTo(arriveImageView.snp.right).offset(30)
+        }
+        arrivalTimeLabel.text = "10:59"
+    }
+    
+    func renderLeavingAtLabelAndLeavingImageView() {
+        containerView.addSubview(youAreLeavingAtLabel)
+        youAreLeavingAtLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(arrivalTimeLabel.snp.bottom).offset(35)
+            make.left.equalToSuperview().offset(15)
+        }
+        youAreLeavingAtLabel.text = "You are leaving at:"
+        youAreLeavingAtLabel.font = youAreLeavingAtLabel.font.withSize(20)
+        
+        containerView.addSubview(leavingImageView)
+        leavingImageView.snp.makeConstraints { (make) in
+            make.top.equalTo(youAreLeavingAtLabel.snp.bottom).offset(15)
+            make.left.equalToSuperview().offset(3)
+            make.width.equalTo(100)
+            make.height.equalTo(100)
+        }
+        leavingImageView.image = UIImage(named: "exit_500x500")
+        leavingImageView.contentMode = .scaleAspectFit
+    }
+    
+    func renderLeavingPickersAndLabelsForDateAndTime() {
+        containerView.addSubview(leavingDateLabel)
+        leavingDateLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(youAreLeavingAtLabel.snp.bottom).offset(40)
+            make.left.equalTo(leavingImageView.snp.right).offset(45)
+        }
+        leavingDateLabel.text = "13.01.2020"
+        
+        containerView.addSubview(leavingTimeLabel)
+        leavingTimeLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(leavingDateLabel.snp.bottom).offset(15)
+            make.left.equalTo(leavingImageView.snp.right).offset(45)
+        }
+        leavingTimeLabel.text = "19:08"
+    }
+    
+    func renderSeparatorAndDescription() {
+        containerView.addSubview(separator)
+        separator.snp.makeConstraints { (make) in
+            make.top.equalTo(leavingTimeLabel.snp.bottom).offset(35)
+            make.left.equalToSuperview().offset(30)
+            make.right.equalToSuperview().inset(30)
+            make.height.equalTo(1)
+        }
+        separator.backgroundColor = UIColor.orange
+        
+        containerView.addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(separator.snp.bottom).offset(40)
+            make.left.equalToSuperview().offset(30)
+            make.right.equalToSuperview().inset(30)
+        }
+        descriptionLabel.text = "You have been working for 3 hours and 15 minutes, would you like to continue?"
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
     }
 
+    func renderCancleAndContinueButton() {
+        containerView.addSubview(stackView)
+        stackView.snp.makeConstraints { (make) in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(15)
+            make.right.equalToSuperview().inset(15)
+            make.height.equalTo(60)
+            make.bottom.equalToSuperview()
+        }
+        stackView.addArrangedSubview(cancleButton)
+        stackView.addArrangedSubview(continueButton)
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+    }
 }
