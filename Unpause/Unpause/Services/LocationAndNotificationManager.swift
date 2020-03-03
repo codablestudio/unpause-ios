0ae9392d2e411b56cb12a9eabfcb18dc4bedb416
Soksijada <k.bakovic7@gmail.com>
2020-03-03 14:35:25 +0100

    Location service is working properly.

---
+++ Unpause/Unpause/Services/LocationAndNotificationManager.swift
@@ -0,0 +1,9 @@
+//
+//  LocationAndNotificationManager.swift
+//  Unpause
+//
+//  Created by Krešimir Baković on 03/03/2020.
+//  Copyright © 2020 Krešimir Baković. All rights reserved.
+//
+
+import Foundation
